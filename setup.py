1f26423261e8b1ef581ae5cc273497187942e557
Jun <chengju@in.tum.de>
2018-10-18 14:57:49 +0200

    add hpa_sr

--- setup.py
+++ setup.py
@@ -1,10 +1,44 @@
-from setuptools import find_packages, setup
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
+requirements = [
+    "scikit-image",
+    "sklearn"
+]
+
+test_requirements = [
+    "pytest",
+    "virtualenv",
+]
 
 setup(
-    name='src',
-    packages=find_packages(),
-    version='0.1.0',
+    name='hpa_src',
+    version='0.0.1',
     description='Human Protein Atlas Kaggle challenge',
     author='Jun Cheng',
-    license='MIT',
+    author_email='...',
+    url='https://github.com/s6juncheng/HumanProteinAtlas',
+    long_description=readme,
+    packages=find_packages(),
+    install_requires=requirements,
+    extras_require={
+        "develop": ["bumpversion",
+                    "wheel",
+                    "jedi",
+                    "epc",
+                    "pytest",
+                    "pytest-pep8",
+                    "pytest-cov"],
+    },
+    entry_points={'console_scripts': ['hpa_src = hpa_src.__main__:main']},
+    license="MIT license",
+    zip_safe=False,
+    test_suite='tests',
+    package_data={},
+    tests_require=test_requirements
 )
