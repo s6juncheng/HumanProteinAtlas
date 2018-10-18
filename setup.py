6a1e122223b6d0f53a1bc0557fcb756134af6146
Jun <chengju@in.tum.de>
2018-10-18 08:16:38 +0200

    init

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Human Protein Atlas Kaggle challenge',
+    author='Jun Cheng',
+    license='MIT',
+)
